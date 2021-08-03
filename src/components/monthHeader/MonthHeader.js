1d3e95270fb24ad278e75e849f01d23e6da00c6e
kfiddle <kenjfiddle@gmail.com>
2021-08-03 19:53:53 -0400

    july to july

--- src/components/monthHeader/MonthHeader.js
+++ src/components/monthHeader/MonthHeader.js
@@ -4,18 +4,19 @@ import SubHeaderMonth from "./SubHeaderMonth";
 import classes from "./MonthHeader.module.css";
 
 const months = [
-  { id: 1, name: "January", clicked: false },
-  { id: 2, name: "February", clicked: false },
-  { id: 3, name: "March", clicked: false },
-  { id: 4, name: "April", clicked: false },
-  { id: 5, name: "May", clicked: false },
-  { id: 6, name: "June", clicked: false },
+ 
   { id: 7, name: "July", clicked: false },
   { id: 8, name: "August", clicked: false },
   { id: 9, name: "September", clicked: false },
   { id: 10, name: "October", clicked: false },
   { id: 11, name: "November", clicked: false },
   { id: 12, name: "December", clicked: false },
+  { id: 1, name: "January", clicked: false },
+  { id: 2, name: "February", clicked: false },
+  { id: 3, name: "March", clicked: false },
+  { id: 4, name: "April", clicked: false },
+  { id: 5, name: "May", clicked: false },
+  { id: 6, name: "June", clicked: false },
 ];
 
 const MonthHeader = (props) => {
