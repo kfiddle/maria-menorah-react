d9b03e9d194b5a51335c0c9a54f788506ab94ef8
kfiddle <kenjfiddle@gmail.com>
2021-08-03 16:18:29 -0400

    getting there with payees

--- src/components/payees/PayeeItem.js
+++ src/components/payees/PayeeItem.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
 
 import classes from "./PayeeItem.module.css";
+import greenCheck from "../../assets/greenCheck1.jpg";
+import redCheck from "../../assets/redCheck1.jpg";
 
 const PayeeItem = (props) => {
   const [clicked, setClicked] = useState(false);
-  const { firstName, lastName } = props.payee;
+  const { firstName, lastName, email, phoneNumber } = props.payee;
 
   const fetchEvents = async () => {
     // fetch("https://bref-chaise-13325.herokuapp.com/get-events-from-payee", {
@@ -22,12 +24,20 @@ const PayeeItem = (props) => {
 
   const clickedPossible = () => {
     fetchEvents();
+    console.log(props.payee);
   };
 
   return (
     <div onClick={clickedPossible} className={classes.payeeItemDiv}>
       <div className={classes.nameDiv}>{`${firstName} ${lastName}`}</div>
-      <div className={classes.leftOverDiv}>nothing yet</div>
+      <div className={classes.emailDiv}>{email}</div>
+      <div className={classes.phoneDiv}>{phoneNumber}</div>
+      <div className={classes.checkMarkDiv}>
+        <img classname={classes.checkMark} src={greenCheck}></img>{" "}
+      </div>
+      <div className={classes.editButtonDiv}>
+        <button>Edit</button>
+      </div>
     </div>
   );
 };
