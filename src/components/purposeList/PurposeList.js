b8cfb1f55fcf35f4ec2b340718f57240f1a48ef8
kfiddle <kenjfiddle@gmail.com>
2021-08-13 14:44:37 -0400

    more on clicked purpose

--- src/components/purposeList/PurposeList.js
+++ src/components/purposeList/PurposeList.js
@@ -1,14 +1,26 @@
+import { useState } from "react";
+
 import PurposeToShow from "./PurposeToShow";
 
 import styles from "./PurposeList.module.css";
 
 const PurposeList = (props) => {
- 
+  const [clickedPurpose, setClickedPurpose] = useState(null);
+
+  const clickedPurposeHandler = (purpose) => {
+      setClickedPurpose(purpose);
+  };
+
   const displayableList = props.list.map((purpose) => (
-    <PurposeToShow purpose={purpose} key={purpose.id} />
+    <PurposeToShow
+      purpose={purpose}
+      clicked={clickedPurposeHandler}
+      highlighted={clickedPurpose === purpose}
+      key={purpose.id}
+    />
   ));
 
-  return <div>{displayableList}</div>;
+  return <div className={styles.purposeListDiv}>{displayableList}</div>;
 };
 
 export default PurposeList;
--- src/components/purposeList/PurposeToShow.js
+++ src/components/purposeList/PurposeToShow.js
@@ -1,8 +1,25 @@
+import { useState } from "react";
+
 import styles from "./PurposeToShow.module.css";
 const PurposeToShow = (props) => {
+  const [purposeClicked, setPurposeClicked] = useState(false);
+
   const { title } = props.purpose;
-  
-  return <div>{title}</div>
+
+  const clickedOrNot = props.highlighted
+    ? `${styles.purposeDiv} ${styles.clickedPurposeDiv}`
+    : styles.purposeDiv;
+
+  const clickedPurpose = () => {
+    setPurposeClicked((previous) => !previous);
+    props.clicked(props.purpose);
+  };
+
+  return (
+    <div onClick={clickedPurpose} className={clickedOrNot}>
+      <h3>{title}</h3>
+    </div>
+  );
 };
 
 export default PurposeToShow;
