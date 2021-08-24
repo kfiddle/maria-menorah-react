9a7bb7485327bd9731e0dc6438dddd9a97ecb2bf
kfiddle <kenjfiddle@gmail.com>
2021-08-24 14:37:37 -0400

    various

--- src/components/purposes/Purpose.js
+++ src/components/purposes/Purpose.js
@@ -1,10 +1,31 @@
+import { Fragment, useState } from "react";
+import PushSomething from "../helperFunctions/PushSomething";
 import styles from "./Purpose.module.css";
 
-
 const Purpose = (props) => {
+  const [itemsList, setItemsList] = useState([]);
   const { title } = props.purpose;
 
-  return <div className={styles.outerContainer}>{title}</div>;
+  const showFoundationItems = async () => {
+    let response = await PushSomething(
+      props.purpose,
+      "/get-foundation-items-from-purpose"
+    );
+    let finalItemsList = await response.json();
+    setItemsList(finalItemsList);
+
+    // let finalShowing = await setItemsList(finalItemsList);
+    // let displayIt = await console.log(itemsList);
+  };
+
+  return (
+    <Fragment>
+      <div onClick={showFoundationItems} className={styles.outerContainer}>
+        {title}
+      </div>
+      {}
+    </Fragment>
+  );
 };
 
 export default Purpose;
