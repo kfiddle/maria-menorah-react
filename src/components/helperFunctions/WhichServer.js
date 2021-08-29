3ef26b341e6cf2a5775bd15b66434350acfef002
kfiddle <kenjfiddle@gmail.com>
2021-08-29 11:13:46 -0400

    styling brighter

--- src/components/helperFunctions/WhichServer.js
+++ src/components/helperFunctions/WhichServer.js
@@ -6,7 +6,7 @@ const WhichServer = () => {
     const realServer = 'https://bref-chaise-13325.herokuapp.com/'
     const fakeServer = 'http://localhost:8080/';
 
-    return realServer;
+    return fakeServer;
 
 }
 
--- src/components/mainNavigation/MainNavigation.js
+++ src/components/mainNavigation/MainNavigation.js
@@ -1,29 +1,46 @@
-
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 import classes from "./MainNavigation.module.css";
 
 const MainNavigation = (props) => {
   return (
     <header className={classes.header}>
-      <div className={classes.logo}><h1>MARIA'S DATABASE</h1></div>
+      <div className={classes.logo}>
+        <h1>MARIA'S DATABASE</h1>
+      </div>
       <nav className={classes.nav}>
         <ul>
-          <li className={classes.navItem} onClick={props.entryClicked}>
-            NEW FOUNDATION ITEM
+          <div className={classes.newFormDiv}>
+            <li className={classes.navItem} onClick={props.entryClicked}>
+              ADD FOUNDATION ITEM
+            </li>
+            <li
+              className={classes.navItem}
+              onClick={props.payeeEntryClicked}
+              style={{ marginRight: "7rem" }}
+            >
+              ADD PAYEE
+            </li>
+          </div>
+
+          <li className={classes.navItem}>
+            <NavLink to={"/foundation-items"}>FOUNDATION ITEMS</NavLink>
           </li>
-          <li className={classes.navItem} onClick={props.payeeEntryClicked} style={{marginRight: '7rem'}}>
-            NEW PAYEE
+          <li className={classes.navItem}>
+            <NavLink to={"/foundations"}>FOUNDATIONS</NavLink>
+          </li>
+          <li className={classes.navItem}>
+            <NavLink to={"/purposes"}>PURPOSES</NavLink>
+          </li>
+          <li className={classes.navItem}>
+            <NavLink to={"/payees"}>ROLODEX</NavLink>
           </li>
 
-          <li className={classes.navItem}><NavLink to={'/foundation-items'}>FOUNDATION ITEMS</NavLink></li>
-          <li className={classes.navItem}><NavLink to={'/foundations'}>FOUNDATIONS</NavLink></li>
-          <li className={classes.navItem}><NavLink to={'/purposes'}>PURPOSES</NavLink></li>
-          <li className={classes.navItem}><NavLink to={'/payees'}>ROLODEX</NavLink></li>
-
-          <li className={classes.navItem}><NavLink to={'/helen-master'}>Helen's Master</NavLink></li>
-          <li className={classes.navItem}><NavLink to={'/stone-garden-master'}>SG Master</NavLink></li>
-        
-     
+          <li className={classes.navItem}>
+            <NavLink to={"/helen-master"}>Helen's Master</NavLink>
+          </li>
+          <li className={classes.navItem}>
+            <NavLink to={"/stone-garden-master"}>SG Master</NavLink>
+          </li>
         </ul>
       </nav>
     </header>
