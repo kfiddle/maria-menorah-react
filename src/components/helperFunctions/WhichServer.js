85173b0747af9ceb57f5da19c75367f1d4e87f73
kfiddle <kenjfiddle@gmail.com>
2021-08-30 22:24:54 -0400

    new backend build

--- src/components/helperFunctions/WhichServer.js
+++ src/components/helperFunctions/WhichServer.js
@@ -1,13 +1,10 @@
-
-
 const WhichServer = () => {
-   
+  // const realServer = 'https://bref-chaise-13325.herokuapp.com/'
 
-    const realServer = 'https://bref-chaise-13325.herokuapp.com/'
-    const fakeServer = 'http://localhost:8080/';
+  const realServer = "https://immense-badlands-06441.herokuapp.com/";
+  const fakeServer = "http://localhost:8080/";
 
-    return fakeServer;
-
-}
+  return realServer;
+};
 
 export default WhichServer;
